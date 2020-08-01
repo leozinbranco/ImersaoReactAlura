c385988b42aed40f9bf8860d5261cfa10bb31008
Leonardo <cyndrasilgames@gmail.com>
2020-08-01 00:41:02 -0300

    arrumei bugs

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
+const jsonServer = require('json-server');
 
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const server = jsonServer.create();
 
+const router = jsonServer.router('db.json');
+
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
-
-server.use(middlewares)
-server.use(router)
-server.listen(port, () => {
-    console.log(`JSON Server is running in ${port}`)
-}) 
+server.use(middlewares);
+server.use(router);
+server.listen(process.env.PORT || port, () => {
+  console.log(`JSON Server is running in ${port}`);
+});

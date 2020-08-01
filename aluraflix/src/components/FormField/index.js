4e0022ed30c9d4af6704b0ff896e01518ab119c8
Leonardo <cyndrasilgames@gmail.com>
2020-07-31 23:26:09 -0300

    Added db and useEffect

--- aluraflix/src/components/FormField/index.js
+++ aluraflix/src/components/FormField/index.js
@@ -1,51 +1,120 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Input = styled.input`
-
+const FormFieldWrapper = styled.div`
+        position: relative;
+    textarea {
+        min-height: 150px;
+    }
+    input[type="color"] {
+        padding-left: 56px;
+    }
 `;
 
-    export default function FormField({type, name ,value, onChange, label, as}) {
-        
-        const fieldID = `id_${name}`;   
-        const tag = type === 'textarea' ? 'textarea': 'input';
-        // if(type === textara) {tag = textarea} else {tag = input}
-        //as = qual vai ser o tipo dele? input?
-        return (
+const Label = styled.label``;
 
-            <div>
-                <label
-                htmlFor={fieldID}
-                >
-                    {label}: 
-                        <Input
-                        as={tag} 
-                        id={fieldID}
-                        type={type}
-                        value={value}//ou values[nome]
-                        name={name}
-                        onChange={onChange}
-                    />
-                </label>
-            </div>
-
-
-        );
-    }
-
+Label.Text = styled.span`
+    color: #E5E5E5;
+    height: 57px;
+    position: absolute; 
+    top: 0;
+    left: 16px;
     
-    FormField.defaultTypes = {
-        type: 'text',  //fica esperando um texto do Type. 
-        value: '',
-        onChange: () => {},//<-- isso significa function, que é o valor padrão(func)
-        as: 'input'
-    };
-    FormField.propTypes = {
-        type: PropTypes.string,
-        name: PropTypes.string.isRequired ,
-        value: PropTypes.string,
-        onChange: PropTypes.func.isRequired,
-        label: PropTypes.string.isRequired,
-        as: PropTypes.string.isRequired,
-    };
+    display: flex;
+    align-items: center;
+    
+    transform-origin: 0% 0%;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 300;
+    
+    transition: .1s ease-in-out;
+`;
+
+const Input = styled.input`
+        background: #53585D;
+        color: #F5F5F5;
+        display: block;
+        width: 100%;
+        height: 57px;
+        font-size: 18px;
+        
+        outline: 0;
+        border: 0;
+        border-top: 4px solid transparent;
+        border-bottom: 4px solid #53585D;
+        
+        padding: 16px 16px;
+        margin-bottom: 45px;
+        
+        resize: none;
+        border-radius: 4px;
+        transition: border-color .3s;
+        
+        &:focus {
+            border-bottom-color: var(--primary);
+        }
+        &:focus:not([type='color']) + span {
+            transform: scale(.6) translateY(-10px);
+        }
+        
+        ${({ hasValue }) => hasValue && css`
+                &:not([type='color']) + span {
+                transform: scale(.6) translateY(-10px);
+                }
+        `};
+        
+`;
+
+export default function FormField({ type, name, value, onChange, label }) {
+
+    const fieldID = `id_${name} `;
+    const isTypeTextArea = type === 'textarea';
+    const tag = isTypeTextArea ? 'textarea' : 'input';
+    // if(type === textara) {tag = textarea} else {tag = input}
+    //as = qual vai ser o tipo dele? input?
+
+    const hasValue = Boolean(value.length);
+    
+    return (
+
+        <FormFieldWrapper>
+            <Label
+                htmlFor={fieldID}
+            >
+                <Input
+                    as={tag}
+                    id={fieldID}
+                    type={type}
+                    value={value}//ou values[nome]
+                    name={name}
+                    onChange={onChange}
+                    hasValue={hasValue}
+                    />
+                    <Label.Text>
+                        {label}
+                        :
+                    </Label.Text>
+            </Label>
+        </FormFieldWrapper>
+
+
+    );
+}
+
+
+FormField.defaultTypes = {
+    type: 'text',  //fica esperando um texto do Type. 
+    value: '',
+    onChange: () => { },//<-- isso significa function, que é o valor padrão(func)
+    as: 'input'
+};
+FormField.propTypes = {
+    type: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired,
+    //as: PropTypes.string.isRequired,
+};
--- aluraflix/src/pages/cadastro/Categoria/index.js
+++ aluraflix/src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import { Link } from 'react-router-dom';
@@ -30,23 +30,58 @@ export default function CadastroCategoria() {
         //const { getAttribute, value } = event.target; //aqui estou pegando apenas o VALUE(event.target.value) e o getAttribute(event.target.getAttribute) do objeto
         //console.log(event.target.getAttribute('name'));
         setValue(
-            event.target.getAttribute('name'),//getAttribute('nome'), 
+            event.target.getAttribute('name'),//getAttribute('nome'), (pegar o props "nome", e pegando o valor dele )
             event.target.value//value 
         );
     }
+
+
+    //sem o array, ele chama o useEffect a todo momento, a cada atualização nao importa o que seja
+    useEffect(() => {
+        console.log("alo alo alo");
+        const url = 'http://localhost:8080/categorias';
+        fetch(url) //fecth = função nativa do browser, que é usada para buscar coisas. essa fetch ela retorna uma promise. (then... etc)
+        .then(async (respostaDoservidor) => {
+            
+            const resposta = await respostaDoservidor.json();
+            setListaCategorias([
+                ...resposta, //para guardar todos os objetos json. 
+            ]);
+        })
+        
+
+
+        /*setTimeout(() => {
+            setListaCategorias([
+                ...listaCategorias,
+                {
+                    id: 1,
+                    nome: 'Front End',
+                    descricao: 'Uma categoria foda',
+                    cor: '#6BD1FF',
+                },
+                {
+                    id: 2,
+                    nome: 'Back End',
+                    descricao: 'Outra categoria foda',
+                    cor: '#6BD1FF',
+                },
+            ]);
+        }, 4 * 1000);*/
+    }, []);
 
     return (
         <PageDefault>
             <h1>Cadastro de Categoria: {values.nome}</h1>
 
             <form
-                style={{ background: values.cor }}
+                //style={{ background: values.cor }}
                 onSubmit={function handleSubmit(event) {
-                    event.preventDefault();
+                    event.preventDefault(); //previne o reloading padrao da pagina 
 
                     setListaCategorias([
                         ...listaCategorias,
-                        values.nome
+                        values
                     ]);
 
                     setValues(valoresIniciais);
@@ -80,15 +115,15 @@ export default function CadastroCategoria() {
             </form>
 
             <ul>
-                {listaCategorias.map((listaCategorias, index) => {
-                    console.log(listaCategorias)
-                    return (
-                        <li key={`${listaCategorias}${index}`}>
-                            {listaCategorias}
 
-                        </li>
-                    );
-                })}
+                {listaCategorias.map((listaCategorias) => (
+                    <li key={`${listaCategorias.id}`}>
+                        {listaCategorias.nome}
+                    </li>
+                )
+
+
+                )}
             </ul>
 
 
